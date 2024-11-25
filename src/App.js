1f43624898bb560bbd155eeb23eb38a5ee2c942d
Eliel García <bgt.eliel@gmail.com>
2024-11-25 14:03:58 -0500

    Añadida ruta a menu-page y enlace en navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import MenuPage from './pages/MenuPage';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/menu-page" element={<MenuPage/>} />
                 {/* Puedes agregar más rutas aquí */}
             </Routes>
         </Router>
